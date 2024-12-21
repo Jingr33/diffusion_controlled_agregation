88dd8c392e65d66c3829940539c007e359fb6551
Jingr33 <jirka.ingr@seznam.cz>
2024-12-21 21:19:44 +0100

    21/12/2024 repair bug - simulation parameter

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ def main():
     parser = argparse.ArgumentParser(description = "Difuzně řízená agregace")
     parser.add_argument("--layout", type=str, choices = layout_choices, default = layout_default, help = "Typ počátečního rozdělení molekul")
     parser.add_argument("--atoms", nargs='+', type=int, default = atoms_default, help = "Počet atomů v simulaci")
-    parser.add_argument("--visualize", type=bool, default=False, help = "Zobrazí vizualizaci počátečního a koncového stavu")
-    parser.add_argument("--plot", type=bool, default=True, help = "Zobrazí graf počet atomů vs. gyrační poloměr")
-    parser.add_argument("--simulation", type=bool, default=True, help = "Spustí simulaci")
+    parser.add_argument("--visualize", action="store_true", help = "Zobrazí vizualizaci počátečního a koncového stavu")
+    parser.add_argument("--plot", action="store_true", help = "Zobrazí graf počet atomů vs. gyrační poloměr")
+    parser.add_argument("--sim", action="store_true", help = "Spustí simulaci")
 
     args = parser.parse_args()
-    _start_sim(args.layout, args.atoms, args.visualize, args.simulation)
+    _start_sim(args.layout, args.atoms, args.visualize, args.sim)
     _plot_chart(args.plot)
 
 def _start_sim (layout : str, atom_numbers : list, visualize : bool, simulation : bool) -> None:
-    if (not simulation): return
+    print(simulation)
+    if not simulation: 
+        return
     for atom_number in atom_numbers:
         sim = Simulation(layout, atom_number, visualize)
         if (visualize):
